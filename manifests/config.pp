905efbbe5bc8c2f232d2c371a1da91d2d7298e6e
Anton Lindström <lindztr@gmail.com>
2013-10-29 10:50:46 +0000

    remove owner and mode for setenv

--- manifests/config.pp
+++ manifests/config.pp
@@ -22,9 +22,6 @@ class crowd::config {
   file { 'setenv.sh':
     ensure => present,
     path   => "/opt/atlassian-crowd-${crowd_version}/apache-tomcat/bin/setenv.sh",
-    mode   => '0755',
-    owner  => root,
-    group  => root,
     source => 'puppet:///modules/crowd/setenv.sh',
   }
 
